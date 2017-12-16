ea010ce27efa4afdc6fff44617c00f96812df4a1
MishkaSimakov <msimakov661@gmail.com>
2017-12-16 18:55:21 +0300

    сделал почти весь цсс

--- resources/error.php
+++ resources/error.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<link rel="stylesheet" type="text/css" href="css/app.css">
-	<meta charset="UTF-8">
-	<title>Уупс!</title>
-</head>
-<body>
 	<?php
-require 'menu/menu.php';
+require 'templates/menu/menu.php';
 	?>
 	
 	<h1>Похоже, что у нас нет этой страницы!</h1>
 
 	<?php
-require 'menu/footer.php';
+require 'templates/menu/footer.php';
 	?>
-</body>
-</html>
--- resources/views/robots/index.php
+++ resources/views/robots/index.php
@@ -13,16 +13,16 @@ $robots = $query->selectAll('robots');
 
 <div class="robots">
 
-    <a href="newrobot.php"><div class="robot">
-        <div class="name-robot">
+    <a href="newrobot.php"><div class="robotinrobots">
+        <div class="name-robotinrobots">
             <h2>Добавить робота</h2>
         </div>
     </div></a>
 
     <?php foreach ($robots as $robot): ?>
-    <a href="show.php"><div class="robot">
-        <div class="img-div-robot"><img class="img-robot" src= "<?= $robot->img_addr ?>" ></div>
-        <div class="name-robot">
+    <a href="show.php"><div class="robotinrobots">
+        <div class="img-div-robot"><img class="img-robotinrobots" src= "<?= $robot->img_addr ?>" ></div>
+        <div class="name-robotinrobots">
             <h2><?= $robot->name ?></h2>
         </div>
     </div></a>
